34f86a884f30620cdca0cfab0378707aeab5ce2b
AMD <takalemanisha131@gmail.com>
2024-10-13 15:51:00 +0530

    Table using printf

--- JavaPractice/Oct09/TableUsingPrintf.java
+++ JavaPractice/Oct09/TableUsingPrintf.java
@@ -27,7 +27,7 @@ int i=1;
         System.out.printf("%d * %d = %d",a, i, a*i);
         i++;System.out.println();
         System.out.printf("%d * %d = %d",a, i, a*i);
-        i++;System.out.println();
+
 
 
     }
