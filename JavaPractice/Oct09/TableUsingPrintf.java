5615927b104a253dd126d9ab01ff993a0531abc5
AMD <takalemanisha131@gmail.com>
2024-10-13 15:47:08 +0530

    Table using printf

---
+++ JavaPractice/Oct09/TableUsingPrintf.java
@@ -0,0 +1,35 @@
+package JavaPractice.Oct09;
+
+public class TableUsingPrintf {
+    public static void main(String[] args) {
+
+        //Table using printf
+int a=10;
+int i=0;
+int n=0 ;
+
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++;n++;System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++;n++;System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++;n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++;System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+        System.out.printf("%d * %d = %d",a, n, a*i);
+        i++; n++; System.out.println();
+
+    }
+}
